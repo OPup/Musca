ceb0fea3bb1b5df3bd97dd883196b5973e92723b
unknown <kjr115@eews104a-022.ic.ac.uk>
2016-05-09 17:56:32 +0100

    updated ram initializer block

---
+++ Game Engine/catapult_projects/maze_ram_tester/MazeRAMTester/MazeArrayTester.v5/res_sharing.tcl
@@ -0,0 +1,4 @@
+directive set /MazeArrayTester/MazeArrayTester:core/MazeArrayTester:core:conc/if:for:for:acc RESOURCE_NAME if:for:mgc_Altera-Cyclone-III-6_beh_psr.mgc_add(6,0,5,0,6)
+directive set /MazeArrayTester/MazeArrayTester:core/MazeArrayTester:core:conc/if:for:acc RESOURCE_NAME if:for:mgc_Altera-Cyclone-III-6_beh_psr.mgc_add(6,0,5,0,6)
+directive set /MazeArrayTester/MazeArrayTester:core/MazeArrayTester:core:conc/if:for:for:acc#1 RESOURCE_NAME if:for:mgc_Altera-Cyclone-III-6_beh_psr.mgc_add(5,0,2,1,5)
+directive set /MazeArrayTester/MazeArrayTester:core/MazeArrayTester:core:conc/if:for:acc#1 RESOURCE_NAME if:for:mgc_Altera-Cyclone-III-6_beh_psr.mgc_add(5,0,2,1,5)
--- Game Engine/src/MazeArrayTester.cpp
+++ Game Engine/src/MazeArrayTester.cpp
@@ -4,13 +4,13 @@
 
 bool written = false;
 
-void MazeArrayTester(ac_int<6, false> *row, ac_int<6, false> *col, ac_int<4, false> *out, bool *write){
+void MazeArrayTester(ac_int<5, false> *row, ac_int<5, false> *col, ac_int<3, false> *out, bool *write){
     if(!written){
         *write = true;
         for(int i = 0; i < ROWS; i++){
             for(int j = 0; j < COLS; j++){
-                *row = (ac_int<6, false>)i;
-                *col = (ac_int<6, false>)j;
+                *row = (ac_int<5, false>)i;
+                *col = (ac_int<5, false>)j;
                 if(j == 0 || j == COLS - 1){
                     *out = WALL;
                 }else if (i == 0 || i == ROWS - 1){
