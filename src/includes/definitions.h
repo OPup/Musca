26e1594b5af47201fb7e021af633442d10dff77e
unknown <kjr115@eews104a-005.ic.ac.uk>
2016-05-11 12:56:46 +0100

    Updated definitions file

--- src/includes/definitions.h
+++ src/includes/definitions.h
@@ -2,8 +2,8 @@
 #include <ac_int.h>
 
 //SIZE definitions
-#define ROWS 64
-#define COLS 64
+#define ROWS 16
+#define COLS 32
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 1024
 #define CELL_SIZE 15
@@ -21,10 +21,10 @@ bool NO_ERROR = false;
 bool ERROR = true;
 
 //MAZE Definitions
-ac_int<4, false>EMPTY_CELL = 0;
-ac_int<4, false>WALL = 1;
-ac_int<4, false>START_POINT_P1 = 2;
-ac_int<4, false>START_POINT_P2 = 3;
-ac_int<4, false>END_POINT = 4;
-ac_int<4, false>P1 = 5;
-ac_int<4, false>P2 = 6;
+ac_int<3, false>EMPTY_CELL = 0;
+ac_int<3, false>WALL = 1;
+ac_int<3, false>START_POINT_P1 = 2;
+ac_int<3, false>START_POINT_P2 = 3;
+ac_int<3, false>END_POINT = 4;
+ac_int<3, false>P1 = 5;
+ac_int<3, false>P2 = 6;
